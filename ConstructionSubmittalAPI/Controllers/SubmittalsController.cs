52561a3e15b4f80062c6ef36cde94cb2b7d5a661
James Barkauskas <barkauskasjames@gmail.com>
2026-03-19 19:50:41 -0500

    Created SubmittalsController

---
+++ ConstructionSubmittalAPI/Controllers/SubmittalsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConstructionSubmittal_API.Controllers
+{
+    [Route("api/submittals")]
+    [ApiController]
+    public class SubmittalsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetSubmittals()
+        {
+            return "Getting all submittals.";
+        }
+
+        [HttpGet("{id:int}")]
+        public string GetSubmittalById(int id)
+        {
+            return "Sub..";
+        }
+    }
+}
