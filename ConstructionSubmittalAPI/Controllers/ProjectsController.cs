cb9849ef108dffce3b8ecad858f022eee3ef1f73
James <barkauskasjames@gmail.com>
2026-03-19 21:16:03 -0500

    Started on ProjectsController, implemented GetProjects method

--- ConstructionSubmittalAPI/Controllers/ProjectsController.cs
+++ ConstructionSubmittalAPI/Controllers/ProjectsController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ConstructionSubmittal_API.Data;
+using ConstructionSubmittal_API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ConstructionSubmittal_API.Controllers
 {
@@ -7,10 +10,18 @@ namespace ConstructionSubmittal_API.Controllers
     [ApiController]
     public class ProjectsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProjects()
+        private readonly AppDbContext _db;
+        public ProjectsController(AppDbContext db)
         {
-            return "Project..";
+            _db = db;
         }
+        
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Project>>> GetProjects()
+        {
+            return Ok(await _db.Projects.ToListAsync());
+        }
+
+
     }
 }
